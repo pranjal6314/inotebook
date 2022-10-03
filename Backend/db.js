03ff58224e76bd462b111b4c2686caa2e0abd14b
Pranjal Choudhary <77271332+pranjal6314@users.noreply.github.com>
2022-10-03 20:59:37 +0530

    iNotebook db & Express Server Setup

---
+++ Backend/db.js
@@ -0,0 +1,17 @@
+const mongoose =require("mongoose");
+const mongoURL="mongodb://localhost:27017";
+
+const connectMongos=()=>{
+
+    mongoose.connect(mongoURL,()=>{
+        console.log("connected to mongo");
+    }).catch((err)=>{
+        console.log(err);
+    })
+
+    //if you want to use async await
+    // await mongosse.connect(mongoURL,{useNewUrlParser:true,useUnifiedTopology:true});
+    // console.log("MongoDB connected");
+}
+
+module.exports=connectMongos;
--- Backend/index.js
+++ Backend/index.js
@@ -1,1 +1,18 @@
-console.log("Hello World")
+
+const connectToMongo = require("./db");
+const express = require('express')
+connectToMongo();
+
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+}).catch((err)=>{
+    console.log(err);
+})
