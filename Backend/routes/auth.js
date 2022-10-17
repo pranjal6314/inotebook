88401d66fd508715bbdae2b6bdb69c1aaf07a19f
Pranjal Choudhary <77271332+pranjal6314@users.noreply.github.com>
2022-10-17 19:32:23 +0530

    Creating Login Endpoint & sending auth token

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -6,7 +6,7 @@ const router=express.Router();
 const User=require("../models/User.js");
 const { body, validationResult } = require('express-validator');
 const JWT_SECRET="Iamgoodboy";
-// creating a User using :Post "/api/auth/CreateUser" . Doesn't require Authentication 
+// creating a User using :Post "/api/auth/CreateUser" . login Doesn't require
 
 router.post("/CreateUser",[
        body('email','enter a valid mail id').isEmail(),
@@ -53,6 +53,47 @@ router.post("/CreateUser",[
       console.log(error.message)
       res.status(500).send("Some Error Occured")
     }
+})
+
+
+// Authenticate a User using :Post "/api/auth/CreateUser" . Doesn't require login 
+
+router.post("/login",[
+  body('email','enter a valid mail id').isEmail(),
+  body('password','password can not be blank').exists(),
+], async (req,res)=>{
+
+     //if there  are error , return bad req and error
+     const errors = validationResult(req);
+     if (!errors.isEmpty()) {
+       return res.status(400).json({ errors: errors.array() });
+     }
+
+     const {email,password}=req.body;
+      try{
+          let user= await User.findOne({email});
+          if(!user){
+            return res.status(400).json({error:"please try to login with correct credentials"})
+          }
+            const passwordCompare=await bcrypt.compare(password,user.password);
+            
+            if(!passwordCompare){
+              return res.status(400).json({error:"please try to login with correct credentials"})
+            };
+            //creating a authtoken for user 
+      const data={
+        user:{
+          id:user.id
+        }
+      }
+      const authtoken= jwt.sign(data,JWT_SECRET);
+   
+      res.json({authtoken});
+      }catch(error){
+        console.log(error)
+        res.status(500).send("Internal Server Error")
+      }
+
 })
 
 module.exports =router;
