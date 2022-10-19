d9fe2fb5db3b46f977151453239fd9853aa0a8de
Pranjal Choudhary <77271332+pranjal6314@users.noreply.github.com>
2022-10-19 23:40:17 +0530

    Creating a middleware to decode user from a JWT -II

---
+++ Backend/middleware/fetchuser.js
@@ -0,0 +1,22 @@
+var  jwt =require('jsonwebtoken');
+const JWT_SECRET="Iamgoodboy";
+const fetchuser = (req, res, next) => {
+    //get the user from jwt token and add id to req object
+    const token =req.header("auth-token");
+    if(!token){
+        res.status(401).send({error:"Please Authenticate using a valid token"});
+    }
+
+    try {
+        const data =jwt.verify(token,JWT_SECRET);
+    req.user=data.user;
+    next();
+    } catch (error) {
+        res.status(401).send({error:"Please Authenticate using a valid token"});
+    }
+        
+    }
+    
+
+
+module.exports=fetchuser;
