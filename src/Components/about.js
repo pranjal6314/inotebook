3fbefe45402f6d25a5e0437794e2644e62af274f
Pranjal Choudhary <pranjal.choudhary2020@vitstudent.ac.in>
2024-01-05 11:59:05 +0530

    devDependencies : adding tailwind css 🔼

--- src/Components/about.js
+++ src/Components/about.js
@@ -1,18 +1,11 @@
 import React from 'react'
-// import { useContext ,useEffect} from 'react';
-// import noteContext from "../Context/notes/noteContext";
 
 
 const About=()=> {
-  // const context=useContext(noteContext);
-  // useEffect(() => {
-  //   context.update();
-  //   // eslint-disable-next-line
-  // },[])
+ 
   return (
     <div>
-      {/* This is an about page! {context.state.name} and he is in {context.state.class} class */}
-      This is an about page! 
+     This is an about page! 
     </div>
   )
 }
--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -44,3 +44,4 @@ function Navbar() {
 }
 
 export default Navbar;
+  
